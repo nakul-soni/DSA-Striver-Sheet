430f0eafe51ec1b0de941eba2179d7ac78b7180b
nakul-soni <nakulsoni2006@gmail.com>
2025-12-07 12:54:00 +0530

    Pattern Commit

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -7,7 +7,6 @@ class patterns {
             System.out.println();
         }
     }
-
     public void printPattern2(int n){
         for(int i = 0; i<n; i++){
             for(int j = 0; j<=i; j++){
